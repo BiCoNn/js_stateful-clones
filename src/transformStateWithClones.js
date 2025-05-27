a648067f10b988f76ea3ba6ad116d8348594e59b
Nazar <nazar.shevchuk.121@gmail.com>
2025-05-27 22:25:53 +0300

    sol1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const finalArr = [];
+  const stateCopy = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(stateCopy, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key in stateCopy) {
+        if (action.keysToRemove.includes(key)) {
+          delete stateCopy[key];
+        }
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in stateCopy) {
+        delete stateCopy[key];
+      }
+    }
+    finalArr.push({ ...stateCopy });
+  }
+
+  return finalArr;
 }
 
 module.exports = transformStateWithClones;
